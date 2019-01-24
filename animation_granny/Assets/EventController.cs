0212bfe8cf601283f7252b1d9a2e68ac15018036
Alice Chen <chenxi26@cs.washington.edu>
2019-01-23 21:18:07 -0800

    Done

---
+++ animation_granny/Assets/EventController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventController : MonoBehaviour {
+
+    public void onposterenter ()
+    {
+        this.GetComponent<Animator>().SetBool("Gaze", true);
+    }
+    public void onposterexit()
+    {
+        this.GetComponent<Animator>().SetBool("Gaze", false);
+    }
+}
---
+++ animation_granny/Assets/Scenes/HandInteraction.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandInteraction : MonoBehaviour {
+
+
+    public Transform Granny;
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 position = this.GetComponent<Transform>().position;
+        Vector3 Grannyposition = Granny.position;
+        if (Vector3.Distance(position,Grannyposition)<0.1)
+        
+{
+            Granny.GetComponent<Animator>().SetBool("Swim", true);
+        } else
+        {
+            Granny.GetComponent<Animator>().SetBool("Swim", false);
+        }
+
+            }
+
+
+}
